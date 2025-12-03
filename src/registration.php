b35661681b25b8d5a4cba9fade406768753dd39f
Mohamed El Mrabet <mimou78@gmail.com>
2025-12-03 08:58:49 +0100

    Adding copyright

--- src/registration.php
+++ src/registration.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * Copyright (c) 2024 Mohamed EL Mrabet
+ * CleatSquad - https://cleatsquad.dev
+ *
+ * This file is part of the CleatSquad_LogStream module.
+ * Licensed under the MIT License. See the LICENSE file in the module root.
+ */
 declare(strict_types=1);
 
 use \Magento\Framework\Component\ComponentRegistrar;
